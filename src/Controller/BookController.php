c9bb7ee90991157e27cbb68e3adde7071e19da17
Smiix <kenjy.thiebault@gmail.com>
2022-09-10 09:49:35 +0200

    feat: read chapters

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Book;
+use App\Entity\Chapter;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,6 +19,25 @@ final class BookController extends AbstractController
     {
         return $this->render('book/index.html.twig', [
             'books' => $entityManager->getRepository(Book::class)->findBy([], limit: 12),
+        ]);
+    }
+
+    #[Route(
+        '/book/{uuid}',
+        name: 'app_book_show',
+        requirements: ['uuid' => '^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$'],
+        methods: Request::METHOD_GET
+    )]
+    public function show(string $uuid, EntityManagerInterface $entityManager): Response
+    {
+        $book = $entityManager->getRepository(Book::class)->find(['uuid' => $uuid]);
+
+        if (null === $book) {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->render('book/show.html.twig', [
+           'chapters' => $entityManager->getRepository(Chapter::class)->findBy(['book' => $book]),
         ]);
     }
 }
--- tests/Functional/BookControllerTest.php
+++ tests/Functional/BookControllerTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional;
 
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use App\Repository\ChapterRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -12,12 +15,28 @@ final class BookControllerTest extends WebTestCase
     /**
      * @test
      */
-    public function shouldDisplayAllBooks(): void
+    public function shouldDisplayTwelveBooks(): void
     {
         $client = self::createClient();
         $crawler = $client->request(Request::METHOD_GET, '/book');
 
         self::assertResponseIsSuccessful();
-        self::assertCount(12, $crawler->filter('div'));
+        self::assertCount(12, $crawler->filter('a'));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldDisplayOneBooksWithThisChapters(): void
+    {
+        $client = self::createClient();
+        /** @var Book $book */
+        $book = $client->getContainer()->get(BookRepository::class)->findOneBy(['title' => 'Title fixture 1']);
+        $chapterCount = count($client->getContainer()->get(ChapterRepository::class)->findBy(['book' => $book]));
+
+        $crawler = $client->request(Request::METHOD_GET, '/book/'.$book->getUuid());
+
+        self::assertResponseIsSuccessful();
+        self::assertCount($chapterCount, $crawler->filter('a'));
     }
 }
