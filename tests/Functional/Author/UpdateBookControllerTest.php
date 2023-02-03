6ca971a2390f2d7fc374cfdacce1ad50fd33f2ab
Smiix <kenjy.thiebault@gmail.com>
2023-02-03 23:18:22 +0100

    style: adr update author book controller

--- tests/Functional/Author/UpdateBookControllerTest.php
+++ tests/Functional/Author/UpdateBookControllerTest.php
@@ -5,13 +5,15 @@ declare(strict_types=1);
 namespace App\Tests\Functional\Author;
 
 use App\Entity\Book;
+use App\Entity\User;
 use App\Repository\BookRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-final class BookControllerTest extends WebTestCase
+final class UpdateBookControllerTest extends WebTestCase
 {
     /**
      * @test
@@ -19,6 +21,9 @@ final class BookControllerTest extends WebTestCase
     public function shouldThrowNotFoundExceptionIfBookIsNotFound(): void
     {
         $client = self::createClient();
+        /** @var User $user */
+        $user = $client->getContainer()->get(UserRepository::class)->findOneBy(['email' => 'user1@email.com']);
+        $client->loginUser($user);
         $client->catchExceptions(false);
 
         self::expectException(NotFoundHttpException::class);
@@ -37,11 +42,36 @@ final class BookControllerTest extends WebTestCase
         $client = self::createClient();
         /** @var Book $book */
         $book = self::getContainer()->get(BookRepository::class)->findOneBy(['title' => 'Title fixture 1']);
+
+        /** @var User $user */
+        $user = $client->getContainer()->get(UserRepository::class)->findOneBy(['email' => 'user1@email.com']);
+        $client->loginUser($user);
         $client->request(Request::METHOD_GET, '/author/book/update/'.$book->getUuid());
         $client->submitForm('Update', $updateBookFormData);
 
         self::assertResponseStatusCodeSame(Response::HTTP_FOUND);
         self::assertNotSame($updateBookFormData['book[title]'], $book->getTitle());
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider provideHttpMethod
+     */
+    public function shouldRedirectToTheLoginPageIfUserIsNotLogin(string $method): void
+    {
+        $client = self::createClient();
+        /** @var Book $book */
+        $book = $client->getContainer()->get(BookRepository::class)->findOneBy(['title' => 'Title fixture 1']);
+        $client->request($method, '/author/book/update/'.$book->getUuid());
+
+        self::assertResponseRedirects();
+        self::assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        self::assertEquals('/login', $client->getRequest()->getPathInfo());
+        self::assertResponseStatusCodeSame(Response::HTTP_OK);
     }
 
     /**
@@ -55,5 +85,14 @@ final class BookControllerTest extends WebTestCase
                 'book[summary]' => 'test content with 20 characters minimum.',
             ],
         ];
+    }
+
+    /**
+     * @return \Generator<array<array-key, string>>
+     */
+    public function provideHttpMethod(): \Generator
+    {
+        yield [Request::METHOD_GET];
+        yield [Request::METHOD_POST];
     }
 }
