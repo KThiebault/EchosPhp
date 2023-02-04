848f50e4dcd41ce88a67d4bc3a6bc7543e532613
Smiix <kenjy.thiebault@gmail.com>
2023-02-04 16:46:09 +0100

    style: adr tag controller

--- src/Controller/Tag/ShowTagController.php
+++ src/Controller/Tag/ShowTagController.php
@@ -2,24 +2,19 @@
 
 declare(strict_types=1);
 
-namespace App\Controller;
+namespace App\Controller\Tag;
 
+use App\Controller\BaseController;
 use App\Entity\Tag;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-final class TagController extends AbstractController
+#[Route('/tag/{tag_uuid}', name: 'app_tag_show', requirements: ['tag_uuid' => self::UUID_REGEX], methods: Request::METHOD_GET)]
+final class ShowTagController extends BaseController
 {
-    #[Route(
-        '/tag/{tag_uuid}',
-        name: 'app_tag_show',
-        requirements: ['tag_uuid' => '^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$'],
-        methods: Request::METHOD_GET
-    )]
-    public function show(string $tag_uuid, EntityManagerInterface $entityManager): Response
+    public function __invoke(string $tag_uuid, EntityManagerInterface $entityManager): Response
     {
         $tag = $entityManager->getRepository(Tag::class)->find(['uuid' => $tag_uuid]);
 
--- tests/Functional/Tag/ShowTagControllerTest.php
+++ tests/Functional/Tag/ShowTagControllerTest.php
@@ -2,14 +2,15 @@
 
 declare(strict_types=1);
 
-namespace App\Tests\Functional;
+namespace App\Tests\Functional\Tag;
 
 use App\Entity\Tag;
 use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-final class TagControllerTest extends WebTestCase
+final class ShowTagControllerTest extends WebTestCase
 {
     /**
      * @test
@@ -24,5 +25,17 @@ final class TagControllerTest extends WebTestCase
         self::assertResponseIsSuccessful();
         self::assertSelectorTextSame('h2', $tag->getName());
         self::assertCount($tag->getBooks()->count(), $crawler->filter('main article'));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldThrowNotFoundExceptionIfTagIsNotFound(): void
+    {
+        $client = self::createClient();
+        $client->catchExceptions(false);
+
+        self::expectException(NotFoundHttpException::class);
+        $client->request(Request::METHOD_GET, '/tag/1ed22f9f-8793-6c00-ad9e-1d77bf6a790b');
     }
 }
