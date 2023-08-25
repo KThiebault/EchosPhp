e0ea250655751c7a38a135465a777032eaa3b0d5
Kenjy Thiébault <43346913+KThiebault@users.noreply.github.com>
2023-08-25 22:11:57 +0200

    feat: add roles admin & super_admin (#31)
    * feat: add roles admin & super_admin (#31)
    
    Signed-off-by: KThiebault <kenjy.thiebault@gmail.com>
    
    * style: change return type
    
    Signed-off-by: KThiebault <kenjy.thiebault@gmail.com>
    
    ---------
    
    Signed-off-by: KThiebault <kenjy.thiebault@gmail.com>

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -21,11 +21,29 @@ final class UserFixture extends Fixture
             $user = new User();
             $user->setEmail(sprintf('user%d@email.com', $index));
             $user->setPseudo(sprintf('user%d', $index));
+            $user->setRoles([User::ROLE_USER]);
             $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
 
             $manager->persist($user);
         }
 
+        for ($index = 1; $index <= 2; ++$index) {
+            $user = new User();
+            $user->setEmail(sprintf('admin%d@email.com', $index));
+            $user->setPseudo(sprintf('admin%d', $index));
+            $user->setRoles([User::ROLE_ADMIN]);
+            $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
+
+            $manager->persist($user);
+        }
+
+        $user = new User();
+        $user->setEmail('super@email.com');
+        $user->setPseudo('super');
+        $user->setRoles([User::ROLE_SUPER_ADMIN]);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
