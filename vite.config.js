14696e414ec39be049231f836f38f0c6e2db5f85
KThiebault <kenjy.thiebault@gmail.com>
2023-08-30 20:51:52 +0200

    feat: install vite and manage docker compose

---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import {defineConfig} from 'vite';
+import {resolve} from 'path'
+
+const twigRefreshPlugin = {
+    name: 'twig-refresh',
+    configureServer ({watcher, ws}) {
+        watcher.add(resolve('templates/**/*.twig'))
+        watcher.on('change', function (path) {
+            if (path.endsWith('.twig')) {
+                ws.send({
+                    type: 'full-reload'
+                })
+            }
+        })
+    }
+}
+
+export default defineConfig({
+    plugins: [twigRefreshPlugin],
+    root: './assets',
+    base: '/assets/',
+    server: {
+        host: true,
+        port: 3000,
+        watch: {
+            disableGlobbing: false
+        }
+    },
+    build: {
+        assetsDir: '',
+        outDir: '../public/assets/',
+        rollupOptions: {
+            input: {
+                'main.js': './assets/main.js'
+            }
+        },
+        manifest: true
+    }
+})
